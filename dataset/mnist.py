bc0ea1b13fc6766cb25e048133af1ac0411d20f4
kakie <782922086@qq.com>
2023-04-03 14:42:37 +0800

    explain for dataset mnist

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -12,10 +12,10 @@ import numpy as np
 
 url_base = 'http://yann.lecun.com/exdb/mnist/'
 key_file = {
-    'train_img':'train-images-idx3-ubyte.gz',
-    'train_label':'train-labels-idx1-ubyte.gz',
-    'test_img':'t10k-images-idx3-ubyte.gz',
-    'test_label':'t10k-labels-idx1-ubyte.gz'
+    'train_img': 'train-images-idx3-ubyte.gz',
+    'train_label': 'train-labels-idx1-ubyte.gz',
+    'test_img': 't10k-images-idx3-ubyte.gz',
+    'test_label': 't10k-labels-idx1-ubyte.gz'
 }
 
 dataset_dir = os.path.dirname(os.path.abspath(__file__))
@@ -29,48 +29,82 @@ img_size = 784
 
 def _download(file_name):
     file_path = dataset_dir + "/" + file_name
-    
+
     if os.path.exists(file_path):
         return
 
     print("Downloading " + file_name + " ... ")
     urllib.request.urlretrieve(url_base + file_name, file_path)
     print("Done")
-    
+
 def download_mnist():
     for v in key_file.values():
        _download(v)
-        
+
+## label pkl 文件的格式
+## 跳过前 8 位 meta 信息，从 0008 开始往后都是真正的标签信息
+## 每个 offset 大小是 32 bytes
+## 详细参考 http://yann.lecun.com/exdb/mnist/ 中的 FILE FORMATS FOR THE MNIST DATABASE
+'''
+TRAINING SET LABEL FILE (train-labels-idx1-ubyte):
+[offset] [type]          [value]          [description]
+0000     32 bit integer  0x00000801(2049) magic number (MSB first)
+0004     32 bit integer  60000            number of items
+0008     unsigned byte   ??               label
+0009     unsigned byte   ??               label
+........
+xxxx     unsigned byte   ??               label
+The labels values are 0 to 9.
+'''
 def _load_label(file_name):
     file_path = dataset_dir + "/" + file_name
-    
+
     print("Converting " + file_name + " to NumPy Array ...")
     with gzip.open(file_path, 'rb') as f:
             labels = np.frombuffer(f.read(), np.uint8, offset=8)
     print("Done")
-    
+
     return labels
 
+## image pkl 文件的格式
+## 跳过前 16 位 meta 信息，从 0016 开始往后都是真正的图片信息
+## 每个 offset 大小是 32 bytes
+'''
+TRAINING SET IMAGE FILE (train-images-idx3-ubyte):
+[offset] [type]          [value]          [description]
+0000     32 bit integer  0x00000803(2051) magic number
+0004     32 bit integer  60000            number of images
+0008     32 bit integer  28               number of rows
+0012     32 bit integer  28               number of columns
+0016     unsigned byte   ??               pixel
+0017     unsigned byte   ??               pixel
+........
+xxxx     unsigned byte   ??               pixel
+Pixels are organized row-wise. Pixel values are 0 to 255. 0 means background (white), 255 means foreground (black).
+'''
 def _load_img(file_name):
     file_path = dataset_dir + "/" + file_name
-    
-    print("Converting " + file_name + " to NumPy Array ...")    
+
+    print("Converting " + file_name + " to NumPy Array ...")
     with gzip.open(file_path, 'rb') as f:
             data = np.frombuffer(f.read(), np.uint8, offset=16)
     data = data.reshape(-1, img_size)
     print("Done")
-    
+
     return data
-    
+
 def _convert_numpy():
     dataset = {}
     dataset['train_img'] =  _load_img(key_file['train_img'])
-    dataset['train_label'] = _load_label(key_file['train_label'])    
+    dataset['train_label'] = _load_label(key_file['train_label'])
     dataset['test_img'] = _load_img(key_file['test_img'])
     dataset['test_label'] = _load_label(key_file['test_label'])
-    
+
     return dataset
 
+"""
+pickle 用来保存一个 KV map 结构，结构值的 img 和 label 中分别存储了单个维度图像的灰度值和结果真值
+"""
 def init_mnist():
     download_mnist()
     dataset = _convert_numpy()
@@ -83,45 +117,45 @@ def _change_one_hot_label(X):
     T = np.zeros((X.size, 10))
     for idx, row in enumerate(T):
         row[X[idx]] = 1
-        
+
     return T
-    
+
 
 def load_mnist(normalize=True, flatten=True, one_hot_label=False):
     """读入MNIST数据集
-    
+
     Parameters
     ----------
     normalize : 将图像的像素值正规化为0.0~1.0
-    one_hot_label : 
+    one_hot_label :
         one_hot_label为True的情况下，标签作为one-hot数组返回
         one-hot数组是指[0,0,1,0,0,0,0,0,0,0]这样的数组
     flatten : 是否将图像展开为一维数组
-    
+
     Returns
     -------
     (训练图像, 训练标签), (测试图像, 测试标签)
     """
     if not os.path.exists(save_file):
         init_mnist()
-        
+
     with open(save_file, 'rb') as f:
         dataset = pickle.load(f)
-    
+
     if normalize:
         for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].astype(np.float32)
             dataset[key] /= 255.0
-            
+
     if one_hot_label:
         dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
         dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
-    
+
     if not flatten:
          for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
 
-    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label']) 
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
 
 
 if __name__ == '__main__':
